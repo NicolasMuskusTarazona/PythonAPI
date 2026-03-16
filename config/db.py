01d59eb72bc2f7eb8b61922bdf8ffa1a7db90f57
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2026-03-15 23:28:59 -0600

    fix: :bug: fix connection

--- config/db.py
+++ config/db.py
@@ -1,10 +1,9 @@
 from pymongo import MongoClient
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 MONGO_URI = os.getenv("MONGO_URI")
-db = MongoClient(MONGO_URI)
+if not MONGO_URI:
+    raise ValueError("No MONGO_URI found in environment variables")
 
-db = db["mydb"]
+conn = MongoClient(MONGO_URI)
+db = conn.get_default_database()
