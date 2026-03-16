33c023ef3929e7265a0f63d93629848dc3cb1561
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2026-03-15 23:41:25 -0600

    test: :test_tube: test connection

--- config/db.py
+++ config/db.py
@@ -6,4 +6,11 @@ if not MONGO_URI:
     raise ValueError("No MONGO_URI found in environment variables")
 
 conn = MongoClient(MONGO_URI)
-db = conn.get_default_database()
+
+try:
+    db = conn.get_default_database()
+    # solo para testear
+    print("Conexion MongoDB OK. Colecciones:", db.list_collection_names())
+except Exception as e:
+    print("❌ Error al conectar MongoDB:", e)
+    raise e
