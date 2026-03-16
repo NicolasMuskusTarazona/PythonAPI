074c08054b6b6d8cf0dcdaa94fe55cf485ec430c
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2026-03-16 11:32:39 -0600

    feat: :sparkles: Endpoint root

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
+import os
+import uvicorn
+
 from routes.user import user
 from routes.character import character
-from fastapi.exceptions import RequestValidationError
-from fastapi import HTTPException
+from routes.auth import auth
+
 from Handler.CustomErrorHandler import (
     custom_http_exception_handler,
     validation_exception_handler,
     generic_exception_handler
 )
+
 from utils.create_admin import create_admin
-from routes.auth import auth
-import os
-import uvicorn
 
 app = FastAPI(
-    title="FastAPI and Mongodb",
-    description="Nicolas Muskus",
-    version="0.0.1",
+    title="CodeGeass API",
+    description="API desarrollada por Nicolas Muskus con FastAPI y MongoDB",
+    version="1.0.0",
 )
 
-# CustomErrorHandler
+# Endpoint 
+@app.get("/")
+def root():
+    return {"message": "CodeGeass API running"}
+
+# Custom Error Handlers
 app.add_exception_handler(HTTPException, custom_http_exception_handler)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
 app.add_exception_handler(Exception, generic_exception_handler)
 
-# Crear admin
+# Crear admin si no existe
 create_admin()
 
 # ROUTERS
+app.include_router(auth)
 app.include_router(user)
 app.include_router(character)
-app.include_router(auth)
 
+# Run local
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
